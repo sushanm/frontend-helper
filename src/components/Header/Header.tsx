c331679e33404de4320a3ca0ea3727c6c14e4949
Sushan M <91654089+Sushan-M@users.noreply.github.com>
2023-06-30 18:05:00 +0530

    updated header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,9 @@ function Header({ heading = "Challenge" }: { heading?: string }) {
       <a href="#/">Home</a>
 
       <h1>{heading[0].toUpperCase() + heading.slice(1)}</h1>
-
+      <a href="https://github.com/sadanandpai/frontend-mini-challenges/" target="_blank" rel="noreferrer">
+        Help Link
+      </a>
       <a href="https://github.com/sushanm/frontend-helper" target="_blank" rel="noreferrer">
         <img
           className={styles.logo}
